8a97415e7f960545695f0a80f689623302931f62
nishchay17 <nishchay.trivedi@theinternetfolks.com>
2021-06-12 20:41:21 +0530

    fix(signup): fixed input field size

--- src/Components/Form/Input.js
+++ src/Components/Form/Input.js
@@ -5,7 +5,6 @@ function Input({ type, name, register, required, placeholder, error }) {
   return (
     <Inputr
       width="100%"
-      height="100%"
       py="0.8rem"
       fontSize="text"
       sx={{
